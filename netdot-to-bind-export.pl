872328dea1b313f998ca18e9a93e2ccf0c1cd3ac
Rod Bio <rjubio@gmail.com>
2017-03-14 10:36:08 +0800

    compare hash of pregi zone

--- netdot-to-bind-export.pl
+++ netdot-to-bind-export.pl
@@ -1,13 +1,16 @@
 #!/usr/bin/perl
 
 use strict;
+use warnings;
 use Digest::SHA qw(sha256_hex);
+use Path::Tiny;
 
 my %self;
 my $zsk = "/etc/bind/keys/Kpregi.net.+013+19150.key";
 my $ksk = "/etc/bind/keys/Kpregi.net.+013+47282.key";
 my $hashdump = "/etc/bind/keys/hashdump.txt";
 my $pregi = "/etc/bind/master/pregi.net" ;
+my $prevhash = "/etc/bind/master/prev-hash" ;
 
 #my $salt = substr(sha256_hex(defined $ARGV[0]?$ARGV[0]:random_hash(8,32)),0,16);
 #open (my $fsalt, '>>', $hashdump) or die "Could not open file '$hashdump' $!";
@@ -15,22 +18,41 @@ my $pregi = "/etc/bind/master/pregi.net" ;
 #print $fsalt " \n";
 #close $fsalt;
 
+### Export zones from netdot
+###  
 print "running netdot exporter.pl... \n";
 if ($? != -1) {
-print "dump success... \n";
 system('/usr/local/netdot/bin/exporter.pl -t BIND -z 128.90.202.in-addr.arpa,129.90.202.in-addr.arpa,131.90.202.in-addr.arpa,132.90.202.in-addr.arpa,149.90.202.in-addr.arpa,158.90.202.in-addr.arpa,134.90.202.in-addr.arpa,133.90.202.in-addr.arpa,130.90.202.in-addr.arpa,153.90.202.in-addr.arpa,phopenix.net,137.90.202.in-addr.arpa,136.90.202.in-addr.arpa,136.90.202.in-addr.arpa,135.90.202.in-addr.arpa,172.32.198.in-addr.arpa');
-
 system('/usr/local/netdot/bin/exporter.pl -t BIND -z pregi.net ');
-print "command success... \n";
-print "signing zone files... \n";
+print "export success... \n";
+
+### To check for updates on the pregi zone
+### compute the md5 hash and compare with 
+### the one on record
+use Digest::MD5::File qw ( file_md5_hex ) ;
+my $md5 = file_md5_hex ( $pregi );
+print "MD5 computed on the zone: $md5 \n";
+open (my $ph, "<$prevhash") or die "Could not open file '$prevhash' $!";
+my $first = <$ph>;
+print "MD5 read from the record: $first \n";
+close $ph;
+if ( $md5 eq $first ) {
+print "Same HASH!!!";
+} else {
 open (my $fh, '>>', $pregi) or die "Could not open file '$pregi' $!";
 print $fh "\$include $zsk \n";
 print $fh "\$include $ksk \n";
 close $fh;
+open (my $ph, '>', $prevhash) or die "Could not open file '$prevhash' $!";
+my $md5 = file_md5_hex ( $pregi );
+print $ph "$md5";
+close $ph;
+print "MD5 written to the record: $md5 \n";
+}
 
-##### Not in use 
-##### Bind doing auto signing now
-##### Ignore this manual signing
+#### Not in use 
+#### Bind doing auto signing now
+#### Ignore this manual signing
 
 #my $cmd = ("/usr/sbin/dnssec-signzone");
 #$cmd .= " \-x \-o pregi.net";
@@ -46,9 +68,6 @@ close $fh;
 #system('/usr/sbin/dnssec-signzone -x -o pregi.net -k $ksk $pregi $zsk');
 #system('/usr/sbin/dnssec-signzone -x -o pregi.net -k /etc/nsd/keys/Kpregi.net.+007+44408.key /etc/nsd/keys/Kpregi.net.+007+58746.key');
 
-
-#system('/usr/sbin/nsdc rebuild');
-#system('/usr/sbin/nsdc reload');
 system('/usr/sbin/rndc reconfig');
 system('/usr/sbin/rndc reload');
 print "notify NS success... \n";
